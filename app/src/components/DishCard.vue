6d9b49624b4aca48f46b09b5ffb698df6b6fbc4b
Wakidou <wakidou.abdallah@gmail.com>
2023-02-09 09:03:50 +0100

    first commit

---
+++ app/src/components/DishCard.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+
+import {defineComponent, computed} from 'vue'
+import type {PropType} from 'vue'
+import type { Dish } from '@/types'
+
+  type PropType = {
+    dish: Dish
+  }
+
+  const props = defineProps<PropType>()
+  const emits = defineEmits<{
+    (e: 'delete-dish', dish:Dish): void,
+    (e: 'edit-dish', dish:Dish): void
+  }>()
+  const statusColor = computed(() =>{
+    switch (props.dish.status) {
+      case 'Want to Try':
+        return 'is-warning'
+      case 'Recommended':
+        return 'is-success'
+      case 'Do Not Recommend':
+        return 'is-danger'
+      default:
+        return ''
+    }
+  })
+  const editDish = () => {
+    emits('edit-dish', props.dish)
+  }
+  const deleteDish = () => {
+    emits('delete-dish', props.dish)
+  }
+</script>
+
+<template>
+  <article class="box">
+    <div class="media">
+      <aside class="media-left">
+        <img src="https://placehold.jp/150x150.png" alt="" />
+      </aside>
+      <div class="media-content">
+        <p class="title is-4 is-spaced mb-1">
+          {{ dish.name }}
+        </p>
+        <p class="subtitle mb-2">
+          <span class="tag" :class="statusColor">{{ dish.status }}</span>
+        </p>
+        <div>
+          <button @click="editDish" class="button is-small is-danger is-light">Ed</button>
+          <button @click="deleteDish" class="button is-small is-danger is-light">Delete</button>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<style></style>
---
+++ app/src/views/EditDishes.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import {ref, computed, onMounted} from 'vue'
+import  { useRoute } from 'vue-router'
+import type { Dish } from '@/types'
+
+
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+</style>
